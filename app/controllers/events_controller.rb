a8476000019499610e2f5aad97cc78d2735d76aa
mattenbar <matthw.enbar@gmail.com>
2020-06-03 19:21:55 -0400

    make event show page and all events are clickable to thier page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,24 @@
 class EventController < ApplicationController
 
-  get '/all' do 
+  get '/event' do 
     @events = Event.all
     erb :'events/all'
   end
 
-  get '/new' do
+  get '/event/new' do
     erb :'events/new'
   end
 
+  post '/event/new' do
+    event = Event.new(params[:event])
+    event.user_id = current_user.id
+    event.save
+    redirect "/event/#{event.id}"
+  end
+
+  get '/event/:id' do
+    id = params[:id]
+    @event = Event.find(id)
+    erb :'/events/show'
+  end
 end
