1716d54af2f2d59fcbfad963a188f7cef33bfad4
mattenbar <matthw.enbar@gmail.com>
2020-06-07 11:07:33 -0400

    can view your favorite events and go to that events page

--- app/models/event.rb
+++ app/models/event.rb
@@ -17,5 +17,26 @@ class Event < ActiveRecord::Base
     @@all_genres_array
   end
 
+
+  def self.find_events_from_favorites
+    @favorite_events = favorite_events.map do |fav| 
+      if fav.event_id != nil
+        Event.find_by(id: fav.event_id)
+      end
+    end
+    @favorite_events.compact
+  end
+
+  def self.favorite_events
+    find_favorite_event_ids
+    @favorite_events = @fav_event_ids.map { |id| FavoriteEvent.find_by(event_id: id) }
+  end
+
+  def self.find_favorite_event_ids
+    @fav_event_ids = FavoriteEvent.all.map { |fav| fav.event_id }
+  end
+
+  
+
   
 end
