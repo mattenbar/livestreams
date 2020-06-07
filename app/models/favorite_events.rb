9598640c8f3a65a6822ce36f93671a29a33b4f9e
mattenbar <matthw.enbar@gmail.com>
2020-06-07 17:19:58 -0400

    fixed favorite event issue to only display current users favorite event on their personal event page

--- app/models/favorite_events.rb
+++ app/models/favorite_events.rb
@@ -2,4 +2,18 @@ class FavoriteEvent < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
 
+
+  def self.favorite_events
+    find_favorite_event_ids
+    @favorite_events = @fav_event_ids.map { |id| FavoriteEvent.find_by(event_id: id)}
+  end
+
+  def self.find_favorite_event_ids
+    @fav_event_ids = FavoriteEvent.all.map { |fav| fav.event_id }
+  end
+
+  def self.currrent_user_favorite_events
+    
+  end
+
 end
